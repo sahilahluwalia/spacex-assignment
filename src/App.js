b8ca726a5ce28ccac4f5556166bdddfd26f79247
sahilahluwalia <44433679+sahilahluwalia@users.noreply.github.com>
2022-10-25 13:04:52 +0530

    basic tailwind setup , react router broke

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { About } from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>nice</div>} />
+        <div>nice</div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+{
+  /* <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <li></li>
+    </div> */
+}
